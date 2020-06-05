6cae9d1cb3e15ff13040d8d827ae93922757cd39
Brendan Regan <bregan@corporatecostcontrol.com>
2020-06-05 15:26:21 -0400

    Add NoteModel

---
+++ NotelyApp/Models/NoteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotelyApp.Models
+{
+    public class NoteModel
+    {
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Detail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
